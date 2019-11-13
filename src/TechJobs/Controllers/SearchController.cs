f2c5ee3e52ebc567adfb865d21423d60cbf60fbf
Edilma Riano <edilmaria@hotmail.com>
2019-11-13 14:20:18 -0500

    initial search result work - needs to fix routes

--- src/TechJobs/Controllers/SearchController.cs
+++ src/TechJobs/Controllers/SearchController.cs
@@ -6,12 +6,32 @@ namespace TechJobs.Controllers
 {
     public class SearchController : Controller
     {
+
+        //List<Dictionary<string, string>> jobs = new List<Dictionary<string, string>>();
         public IActionResult Index()
         {
             ViewBag.columns = ListController.columnChoices;
             ViewBag.title = "Search";
             return View();
         }
+
+        [HttpPost]
+        [Route ("/Search")]
+
+    public IActionResult Results( string column, string searchTerm) // check names 
+        {
+            ViewBag.title = "Search by Term ";
+            ViewBag.columns = ListController.columnChoices;
+            searchTerm = searchTerm.ToLower();
+            List<Dictionary<string, string>> jobs = JobData.FindByColumnAndValue(column, searchTerm);
+
+            ViewBag.jobs = jobs;
+            return View("Index");
+
+                            }
+
+
+
 
         // TODO #1 - Create a Results action method to process 
         // search request and display results
