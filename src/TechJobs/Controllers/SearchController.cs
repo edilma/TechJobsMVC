6de4b9a0a0e505168b74edf04aeda81331b0282d
Edilma Riano <edilmaria@hotmail.com>
2019-11-14 18:07:50 -0500

    Search results working.  Need to do second part

--- src/TechJobs/Controllers/SearchController.cs
+++ src/TechJobs/Controllers/SearchController.cs
@@ -15,15 +15,14 @@ namespace TechJobs.Controllers
             return View();
         }
 
-        [HttpPost]
-        [Route ("/Search")]
-
-    public IActionResult Results( string column, string searchTerm) // check names 
+    
+        //http://localhost:55958/Search/Results?searchType=location&searchTerm=portland
+        public IActionResult Results( string searchType, string searchTerm) // check names 
         {
             ViewBag.title = "Search by Term ";
             ViewBag.columns = ListController.columnChoices;
             searchTerm = searchTerm.ToLower();
-            List<Dictionary<string, string>> jobs = JobData.FindByColumnAndValue(column, searchTerm);
+            List<Dictionary<string, string>> jobs = JobData.FindByColumnAndValue(searchType, searchTerm);
 
             ViewBag.jobs = jobs;
             return View("Index");
